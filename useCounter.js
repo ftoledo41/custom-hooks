783fecf5c0fd21adfeea15f4a7c93c927b544d0f
ftoledo43 <franciscojaviertoledocollante@gmail.com>
2024-12-20 12:24:09 -0300

    Primer commit

---
+++ useCounter.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export const useCounter = (initialValue = 10) => {
+  const [counter, setCounter] = useState(initialValue);
+
+  // Varias maneras de exponer hacia el exterior
+  const increment = (value = 1) => {
+    setCounter(counter + value);
+  };
+
+  const decrement = () => {
+    if (counter == 0) return;
+
+    setCounter(counter - 1);
+  };
+
+  const reset = () => {
+    setCounter(initialValue);
+  };
+
+  return {
+    counter,
+    increment,
+    decrement,
+    reset,
+  };
+};
